46d48c5d61268046044b26e2ae8b5c872d0bfba7
AdibahShah <adibahshah@hotmail.com>
2016-02-11 12:37:41 +0800

    Create phpjobs.sh

---
+++ project-data/phpjobs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+/usr/bin/php /var/www/rsavisual.com/htdocs/project-data/delete.php
+/usr/bin/php /var/www/rsavisual.com/htdocs/project-data/connector.php
+/usr/bin/php /var/www/rsavisual.com/htdocs/project-data/pass.php
